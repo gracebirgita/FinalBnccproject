87f165c40f15c37cc109825b456b05208a8298da
adiguna sanjaya L <115352621+adxze@users.noreply.github.com>
2024-07-21 01:04:26 +0800

    app

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Session\Session;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session as FacadesSession;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        FacadesSession::flash('email', $request->email);
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ], [
+            'email.required' => 'Email wajib diisi',
+            'password.required' => 'Password wajib diisi',
+        ]);
+
+        $infologin = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+        if (Auth::attempt($infologin)) {
+            if (Auth::user()->email == 'admin@gmail.com') {
+                return redirect('/admindashboard')->with('success', 'Login berhasil');
+            }
+            else {
+                return redirect('/dashboard')->with('success', 'Login berhasil');
+            }
+        } else {
+            return redirect('/login')->with('error', 'Email atau password salah');
+        }
+    }
+
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/login');
+    }
+}
+
+
---
+++ app/helpers.php
@@ -0,0 +1,10 @@
+<?php
+
+// app/helpers.php
+
+if (!function_exists('formatRupiah')) {
+    function formatRupiah($amount)
+    {
+        return 'Rp ' . number_format($amount, 0, ',', '.');
+    }
+}
